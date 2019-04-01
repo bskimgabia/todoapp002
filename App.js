5fac9a3e4c8e023db4b85d1e7461b3e2fb58d630
Byeong Sang Kim <bskim@gabia.com>
2019-04-01 21:05:15 +0900

    2nd Commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Dimensions,
+  Platform
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+const { heigh, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>To Do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"placeholder"} />
+        </View>
       </View>
     );
   }
@@ -14,8 +28,28 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F23657",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "400",
+    marginTop: 30,
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: { shadowColor: "rgb(50, 50, 50)" },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
